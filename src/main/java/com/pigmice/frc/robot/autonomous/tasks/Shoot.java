1175cc844ac8006f26a09868c60fc73ddcc110b3
Brendan Burkhart <blackravenbrendan@gmail.com>
2020-02-21 23:59:07 -0800

    Simplify shooter spinup action

--- src/main/java/com/pigmice/frc/robot/autonomous/tasks/Shoot.java
+++ src/main/java/com/pigmice/frc/robot/autonomous/tasks/Shoot.java
@@ -1,6 +1,7 @@
 package com.pigmice.frc.robot.autonomous.tasks;
 
 import com.pigmice.frc.robot.subsystems.Feeder;
+import com.pigmice.frc.robot.subsystems.Feeder.LiftAction;
 import com.pigmice.frc.robot.subsystems.Shooter;
 
 import edu.wpi.first.wpilibj.Timer;
@@ -9,13 +10,10 @@ public class Shoot implements ITask {
     private final Shooter shooter;
     private final Feeder feeder;
 
-    private final double shootingLength = 1.2;
+    private final double shootLength = 2.0;
 
-    private double shootStartTime = 0.0;
+    private double startTime = 0.0;
     private boolean shootingStarted = false;
-
-    private double shooterReadyTime = 0.0;
-    private boolean shooterStable = false;
 
     public Shoot(Shooter shooter, Feeder feeder) {
         this.shooter = shooter;
@@ -24,47 +22,32 @@ public class Shoot implements ITask {
 
     @Override
     public void initialize() {
-        shootStartTime = 0.0;
+        startTime = 0.0;
         shootingStarted = false;
-
-        shooterReadyTime = 0.0;
-        shooterStable = false;
     }
 
     @Override
     public boolean update() {
         shooter.run(true);
 
-        if (!shooter.isReady() && !shooterStable) {
-            shootingStarted = false;
-            return false;
-        }
-
-        if (!shootingStarted && shooter.isReady()) {
+        if (shooter.isReady() && !shootingStarted) {
             shootingStarted = true;
-            shooterStable = false;
-            shootStartTime = Timer.getFPGATimestamp();
+            startTime = Timer.getFPGATimestamp();
         }
 
-        if (!shooterStable && Timer.getFPGATimestamp() - shootStartTime > 0.5) {
-            shooterStable = true;
-            shooterReadyTime = Timer.getFPGATimestamp();
+        if(shootingStarted) {
+            feeder.runLift(LiftAction.FEED);
+            feeder.runHopper(true);
         }
 
-        if (shooterStable && Timer.getFPGATimestamp() - shooterReadyTime > shootingLength) {
+        if (shootingStarted && Timer.getFPGATimestamp() - startTime > shootLength) {
             shooter.run(false);
-            feeder.runLift(false);
+            feeder.runLift(LiftAction.HOLD);
             feeder.runHopper(false);
 
             return true;
         }
 
-        if (shooterStable) {
-            feeder.runLift(true);
-            feeder.runHopper(true);
-        }
-
         return false;
     }
-
 }
