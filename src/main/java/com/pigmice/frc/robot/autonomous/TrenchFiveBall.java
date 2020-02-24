c3428b76e8ffd5d5659a12bc35f1f8b4770911fc
Brendan Burkhart <blackravenbrendan@gmail.com>
2020-02-23 21:19:36 -0800

    Tune five ball auto routine

--- src/main/java/com/pigmice/frc/robot/autonomous/TrenchFiveBall.java
+++ src/main/java/com/pigmice/frc/robot/autonomous/TrenchFiveBall.java
@@ -22,7 +22,7 @@ public class TrenchFiveBall extends Autonomous {
         PathFollower acquisition = constructAcquisition(drivetrain, intake, feeder);
         PathFollower returnAndSpinUp = constructReturnPath(drivetrain, shooter, intake, feeder);
 
-        this.subroutines = Arrays.asList(acquisition, returnAndSpinUp, new Shoot(shooter, feeder));
+        this.subroutines = Arrays.asList(acquisition, returnAndSpinUp, new Shoot(shooter, feeder, 2.0));
     }
 
     public void initialize() {
@@ -36,19 +36,23 @@ public class TrenchFiveBall extends Autonomous {
     public static PathFollower constructAcquisition(Drivetrain drivetrain, Intake intake, Feeder feeder) {
         List<Point> positions = new ArrayList<>();
         positions.add(new Point(0.0, 0.0));
-        positions.add(new Point(0.5, -0.5));
-        positions.add(new Point(-1.0, -1.0));
-        positions.add(new Point(-1.5, -2.75));
+        positions.add(new Point(-0.3, -0.45));
+        positions.add(new Point(-0.5, -0.6));
+        positions.add(new Point(-0.5, -0.8));
+        positions.add(new Point(-1.25, -1.85));
+        positions.add(new Point(-1.4, -2.1));
 
         List<Double> velocities = new ArrayList<>();
-        velocities.add(3.0);
-        velocities.add(3.0);
-        velocities.add(2.5);
+        velocities.add(1.0);
+        velocities.add(2.0);
+        velocities.add(1.5);
+        velocities.add(1.3);
+        velocities.add(0.9);
         velocities.add(0.75);
 
         Path path = new Path(positions, velocities);
 
-        IAction powerCellAcquisition = new Acquire(intake, feeder);
+        IAction powerCellAcquisition = new Acquire(intake, feeder, true);
 
         PathFollower follower = new PathFollower(drivetrain, path, true, 2.0);
         follower.addAction(0, 2, powerCellAcquisition);
@@ -58,17 +62,21 @@ public class TrenchFiveBall extends Autonomous {
     public static PathFollower constructReturnPath(Drivetrain drivetrain, Shooter shooter, Intake intake,
             Feeder feeder) {
         List<Point> positions = new ArrayList<>();
-        positions.add(new Point(-1.5, -2.75));
-        positions.add(new Point(0.5, -0.5));
-        positions.add(new Point(2.5, -0.5));
-        positions.add(new Point(3.0, -0.0));
-        positions.add(new Point(3.0, 0.5));
-        positions.add(new Point(3.0, 1.0));
+        positions.add(new Point(-1.365, -2.05));
+        positions.add(new Point(-1.325, -1.25));
+        positions.add(new Point(1.0, -0.5));
+        positions.add(new Point(1.5, -0.5));
+        positions.add(new Point(3.0, -0.5));
+        positions.add(new Point(3.25, -0.0));
+        positions.add(new Point(3.4, 0.5));
+        positions.add(new Point(3.5, 1.0));
 
         List<Double> velocities = new ArrayList<>();
-        velocities.add(1.0);
-        velocities.add(1.0);
-        velocities.add(1.0);
+        velocities.add(2.0);
+        velocities.add(2.0);
+        velocities.add(2.0);
+        velocities.add(2.0);
+        velocities.add(1.75);
         velocities.add(1.0);
         velocities.add(1.0);
         velocities.add(0.75);
@@ -77,7 +85,7 @@ public class TrenchFiveBall extends Autonomous {
 
         IAction spinUp = new SpinUp(shooter, feeder);
         IAction backFeed = new BackFeed(feeder);
-        IAction powerCellAcquisition = new Acquire(intake, feeder);
+        IAction powerCellAcquisition = new Acquire(intake, feeder, false);
 
         PathFollower follower = new PathFollower(drivetrain, path, false, 1.0);
         follower.addAction(0, 1, powerCellAcquisition);
